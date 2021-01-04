12aa3f20158d1d68dfbb3b3d912fb6c7641c1b17
Muh. Shamad SST <shamad2402@gmail.com>
2021-01-04 10:07:08 +0800

    tahun cron

--- cron/nomor_kosong_mingguan.js
+++ cron/nomor_kosong_mingguan.js
@@ -16,6 +16,7 @@ var nomor_kosong_mingguan = schedule.scheduleJob(senin_rabu_jumat_jam12malam, ()
                 SuratKeluar.create({
                     _id: `${tahun_terpilih}_${nomor_baru}`,
                     nomor: nomor_baru,
+                    tahun: tahun_terpilih,
                     // tgl_surat: new Date(),
                     nomor_kosong: true
                 }, (err, suratKeluarKosongMingguan) => {
