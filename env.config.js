52032b74a4e5b3a6d3651c24d8a8f84ef87584ed
Muh. Shamad SST <shamad2402@gmail.com>
2025-06-15 06:36:20 +0800

    Update environment configuration to use production URLs for SSO and SISUKMA services

--- env.config.js
+++ env.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   JWT_SECRET_PROD : '@j4nzky94%@$',
   JWT_SECRET_DEV : '@j4nzky94%@$',
-  SSO_HOST: 'http://localhost:85',
-  SSO_USER_API_HOST: 'http://localhost:84',
-  SISUKMA_HOST: 'http://localhost:83',
+  // SSO_HOST: 'http://localhost:85',
+  // SSO_USER_API_HOST: 'http://localhost:84',
+  // SISUKMA_HOST: 'http://localhost:83',
+  SSO_HOST: 'https://sso.bpskolaka.com',
+  SSO_USER_API_HOST: 'https://user-api.bpskolaka.com',
+  SISUKMA_HOST: 'https://sisukma.bpskolaka.com',
   // HOST: '127.0.0.1',
   HOST: 'host.docker.internal',
 }
